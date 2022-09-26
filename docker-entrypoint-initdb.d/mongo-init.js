2d7ab979281d26419c99fc203931318f975f7f2c
Cavelier Mickael <cavelier.mickael@cookie-numerique.fr>
2022-09-26 17:25:11 +0200

    docker init db

---
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -0,0 +1,22 @@
+
+print('Start #################################################################');
+db = db.getSiblingDB("mirabelle");
+
+db.createUser({
+  user: process.env.MONGO_USER_MIKA,
+  pwd:  process.env.MONGO_PASSWORD_MIKA,
+  roles: [{ role: 'root', db: 'admin' }],
+});
+
+db.createUser({
+  user: process.env.MONGO_USER_MARIE,
+  pwd: process.env.MONGO_DB_MARIE,
+  roles: [
+    {
+      role: "read",
+      db: "mirabelle",
+    },
+  ],
+});
+
+print('END #################################################################');
