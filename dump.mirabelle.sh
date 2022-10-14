5319b9131e0d858c249fb88c9b99a529e0ae2520
Cavelier Mickael <cavelier.mickael@cookie-numerique.fr>
2022-10-14 09:53:46 +0200

    maj script sheel

--- dump.mirabelle.sh
+++ dump.mirabelle.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+pathEnv=/opt/docker-apps/mongo/.env;
 
-if [ -f .env ]; then
-  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+if [ -f $pathEnv ]; then
+  export $(echo $(cat $pathEnv | sed 's/#.*//g'| xargs) | envsubst)
 fi
 
 docker exec -ti --user www-data mongodb mongodump -u $MONGO_USER_BACKUP -p $MONGO_PASS_BACKUP --authenticationDatabase admin -d mirabelle -o /database-backup --ssl --tlsInsecure;
